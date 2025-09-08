ae282b93d9f47b6f37244a7bdbe42ee54ee9cf3c
ashvinikumargautam <ashvini1275@gmail.com>
2025-09-08 17:08:39 +0530

    8 sep 2025

--- l94 Ecercise 8 alarm clock/l94.js
+++ l94 Ecercise 8 alarm clock/l94.js
@@ -1,16 +1,43 @@
-// function play(){
-//     // var audio =new Audio("Alarm Sound Effect.mp3")
-//     audio.play();
-// }
-// setTimeout(()=>{
-//     play()
-// },1000)
-// play()
-let p =document.getElementById("h2")
+function play() {
+    var audio = new Audio("Alarm Sound Effect.mp3"); // Ensure file is in the correct path
+    audio.play();
+}
 
-let x =  setInterval(()=>{
-    let p = new Date()
-    var times = `time is : ${p.getHours()}:${p.getMinutes()}:${p.getSeconds()}`
-    var dates = `date is : ${p.getDate()}:${p.getMonth()}:${p.getFullYear()}`
-    p.document.write = p.getTime()
-},1000)
+setInterval(() => {
+    let ti = document.getElementById("time");
+    let d = new Date();
+    if (ti) {
+        ti.innerHTML = d.toDateString() + " " + d.toTimeString();
+    }
+}, 1000);
+
+const setAlarm = (seconds) => {
+    let startTime = new Date().getTime();
+    setTimeout(() => {
+        let alar = document.getElementById("alarm");
+        if (alar) {
+            play();
+            alar.innerHTML = "Alarm is ringing NOW!";
+        }
+    }, seconds * 1000);
+
+    let interval = setInterval(() => {
+        let alar = document.getElementById("alarm");
+        let now = new Date().getTime();
+        let secondsLeft = Math.floor((startTime + seconds * 1000 - now) / 1000);
+        if (alar) {
+            if (secondsLeft > 0) {
+                alar.innerHTML = "Alarm will ring in " + secondsLeft + " seconds";
+            } else {
+                clearInterval(interval);
+            }
+        }
+    }, 100);
+};
+ document.getElementById("start").addEventListener("click", function() {
+  let s = prompt("After how many seconds do you want the alarm?");
+  setAlarm(parseInt(s));
+});
+
+// let s = prompt("After how many seconds do you want the alarm?");
+// setAlarm(parseInt(s));
